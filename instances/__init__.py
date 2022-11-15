415e64750a089a28075b9c7d99d8dda5559abf5f
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-11-15 17:06:16 +0100

    Corrected n queens

---
+++ instances/__init__.py
@@ -0,0 +1,1 @@
+from .n_queens_instance import n_queens_problem
--- instances/n_queens_instance.py
+++ instances/n_queens_instance.py
@@ -15,7 +15,7 @@ def n_queens_problem(n: int) -> CSP:
     # The way our CSP is coded, one will have to get the domain and constraints
     # by checking if a string is a key of a dict. So we put the part that changes
     # from a queen to the next first, in order to check more quickly
-    variables = [f"{str(i)}_queen_col" for i in range(1, n + 1)]
+    variables = [f"{str(i)}_col_queen" for i in range(1, n + 1)]
     # Each domain is [1, ..., n]
     common_domain = list(range(1, n + 1))
     domains = {variable: common_domain for variable in variables}
@@ -23,11 +23,11 @@ def n_queens_problem(n: int) -> CSP:
     csp_queen = CSP(variables=variables, domains=domains, constraints={})
 
     # Constraint Var_i != Var_j
-    csp_queen.add_constraints(alldiff(csp_instance=CSP))
+    csp_queen.add_constraints(alldiff(csp_instance=csp_queen))
     # Constraint : for all i < j,  Var_i - Var_j != j - i
     csp_queen.add_constraints(
         lambda_wrapper_all_vars_tuples(
-            csp_instance=CSP,
+            csp_instance=csp_queen,
             boolean_lambda=(
                 lambda i, j, value_var_i, value_var_j: (value_var_i - value_var_j)
                 != j - i
@@ -37,7 +37,7 @@ def n_queens_problem(n: int) -> CSP:
     # Constraint : for all i < j,  Var_j - Var_i != j - i
     csp_queen.add_constraints(
         lambda_wrapper_all_vars_tuples(
-            csp_instance=CSP,
+            csp_instance=csp_queen,
             boolean_lambda=(
                 lambda i, j, value_var_i, value_var_j: (value_var_j - value_var_i)
                 != j - i
