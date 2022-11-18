72b3076f5200e68a48e316f5b618d7c944fa9634
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2022-11-18 16:25:38 +0100

    Modified csp

--- constants/custom_types_definitions.py
+++ constants/custom_types_definitions.py
@@ -1,12 +1,18 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Callable
 
 # Variables
 Variable = str
 Variables = list[str]
 VariableValue = Union[float, int]
+
 # domains
 Domain = list[VariableValue]
-Domains = dict[Variable, Domain]
-# Variables
-Constraint = set[Tuple[VariableValue, VariableValue]]
-Constraints = dict[Tuple[Variable, Variable], Constraint]
+# The domains are now just a list of domains are the keys would be the index of the associated variable.
+Domains = list[Domain]
+
+# constraints
+# A constraint is now a Callable that takes for values, the indices of the two variables to test on and the two
+# associated values.
+Constraint = Callable[[int, int, VariableValue, VariableValue], bool]
+# Constraints are stored in a dict whose keys are the tuple of the indices i and j of the variable it constricts
+Constraints = dict[Tuple[int, int], Constraint]
--- models/csp.py
+++ models/csp.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from constants import Domains, Constraints, Variable, Variables, Constraint
 
 
@@ -8,14 +10,17 @@ class CSP:
             instead of only indices (thinking for instance of the problem of Lewis Caroll's zebra where variables
             would be treated easily by giving them the name of the attributes they represented). They are stored in
             a list
-        - each domain is a list of the possible values taken. They are stored in a dict since
-            our variables are strings.
-        - each constraint is a set of tuples of admissibles variables values for the variables
-            of the constraint. They are stored in a dict whose key is the tuple (variable_1, variable_2)
-            and whose value is the possible values of said variables.
+        - domains are stored in a list, domains[i] being the domain of the variable C_i.
+        - each constraint is a lambda function to evaluate the values of two variables and know wether they are
+            valid or not. They are stored in a dict whose key is the tuple (index_variable_1, index_variable_2)
+            and whose value is the lambda function. We ensure we always have index_variable_1 < index_variable_2 for the
+            stored constraints.
 
+    When building the CSP we also create a dict which maps variables (as strings) to their index in the list. This is used
+    to provide easier functions where one would for instance build a constraint on "Apple" and "Pear" rather than 1 and 14.
     """
 
+    variables_to_index_dict: dict
     variables: Variables
     domains: Domains
     constraints: Constraints
@@ -29,6 +34,9 @@ class CSP:
         self.variables = variables
         self.domains = domains
         self.constraints = constraints
+        self.variables_to_index_dict = {
+            variables[index]: index for index in range(len(variables))
+        }
 
     def __str__(self):
         """
@@ -37,50 +45,116 @@ class CSP:
         str_representation = f"Variables:\n{self.variables}\n"
 
         str_representation += "\nDomains:\n"
-        for variable, domain in self.domains.items():
-            str_representation += f"{variable} : {domain}\n"
+        for index in range(len(self.variables)):
+            str_representation += f"{self.variables[index]} : {self.domains[index]}\n"
 
         str_representation += "\nConstraints:\n"
-        for var_tuple, constraint in self.constraints.items():
-            str_representation += f"{var_tuple} : {constraint}\n"
+        for (i, j), _ in self.constraints.items():
+            str_representation += (
+                f"{(self.variables[i], self.variables[j])}  have constraints.\n"
+            )
 
         return str_representation
 
+    def get_current_constraint_if_exists(
+        self, index_variable_1: int, index_variable_2: int
+    ) -> Constraint:
+        """
+        Returns the current constraint if it exists, None otherwise.
+        """
+        return self.constraints.get((index_variable_1, index_variable_2), None)
+
+    def swap_indices_if_needed(
+        self,
+        index_variable_1: int,
+        index_variable_2: int,
+        new_constraint: Constraint,
+    ) -> Tuple[int, int, Constraint]:
+        """
+        We want to have and keep index_variable_1 < index_variable_2. Swap them and rebuild the
+        constraint.
+        """
+        # If needed, swap
+        if index_variable_1 > index_variable_2:
+            swapped_constraint: Constraint = (
+                lambda i, j, value_var_i, value_var_j: new_constraint(
+                    j, i, value_var_j, value_var_i
+                )
+            )
+            return (index_variable_2, index_variable_1, swapped_constraint)
+        else:
+            return index_variable_1, index_variable_2, new_constraint
+
+    def combine_two_constraints(
+        current_constraint: Constraint, new_constraint: Constraint
+    ) -> Constraint:
+        """
+        This function combines two constraint on the same variables to build a new one.
+        """
+        return lambda i, j, value_var_i, value_var_j: current_constraint(
+            i, j, value_var_i, value_var_j
+        ) and new_constraint(i, j, value_var_i, value_var_j)
+
     def add_constraint(
-        self, variable_1: Variable, variable_2: Variable, new_constraint: Constraint
+        self,
+        index_variable_1: Variable,
+        index_variable_2: Variable,
+        new_constraint: Constraint,
     ) -> None:
         """
         Add a single constraint to the CSP. If no constraint exists on the variables, just put
         it in the dict. Else, intersect with current constraint.
         """
-        # Test first possible order
+        # Get the indices in the right order if not already the case
+        (
+            index_variable_1,
+            index_variable_2,
+            new_constraint,
+        ) = self.swap_indices_if_needed(
+            index_variable_1=index_variable_1,
+            index_variable_2=index_variable_2,
+            new_constraint=new_constraint,
+        )
+        # Get current constraint if it exists.
         if (
-            current_constraint := self.constraints.get((variable_1, variable_2), None)
+            current_constraint := self.get_current_constraint_if_exists(
+                index_variable_1=index_variable_1, index_variable_2=index_variable_2
+            )
         ) is not None:
-            # If the current one exist, intersect current and new.
+            # If the current one exists, make an and with new one.
             self.constraints[
-                (variable_1, variable_2)
-            ] = current_constraint.intersection(new_constraint)
-        # Then second order
-        elif (
-            current_constraint := self.constraints.get((variable_2, variable_1), None)
-        ) is not None:
-            # If the current one exist, intersect current and new.
-            self.constraints[
-                (variable_2, variable_1)
-            ] = current_constraint.intersection(new_constraint)
+                (index_variable_1, index_variable_2)
+            ] = self.combine_two_constraints(
+                current_constraint=current_constraint, new_constraint=new_constraint
+            )
         else:
             # If no current constraint exists, then just put the new constraint as is.
-            self.constraints[(variable_1, variable_2)] = new_constraint
+            self.constraints[(index_variable_1, index_variable_2)] = new_constraint
 
         return
 
-    def add_constraints(self, new_constraints: dict) -> None:
+    def add_constraints_with_indices(self, new_constraints: dict) -> None:
         """
-        Adds a new bunch of constraints stored as a dict in the CSP.
+        Adds a new bunch of constraints stored as a dict in the CSP. The keys are indices.
+        """
+        for (index_variable_1, index_variable_2), constraint in new_constraints.items():
+            self.add_constraint(
+                index_variable_1=index_variable_1,
+                index_variable_2=index_variable_2,
+                new_constraint=constraint,
+            )
+        return
+
+    def add_constraint_with_variables(self, new_constraints: dict) -> None:
+        """
+        Adds a new bunch of constraints stored as a dict in the CSP. The keys are variables.
         """
         for (variable_1, variable_2), constraint in new_constraints.items():
+            index_variable_1 = self.variables_to_index_dict[variable_1]
+            index_variable_2 = self.variables_to_index_dict[variable_2]
             self.add_constraint(
-                variable_1=variable_1, variable_2=variable_2, new_constraint=constraint
+                index_variable_1=index_variable_1,
+                index_variable_2=index_variable_2,
+                new_constraint=constraint,
             )
         return
